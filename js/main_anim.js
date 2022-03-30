7da2f9222af32bc9965b2b5caf37a05512ce412c
Roman Yaremenko <romanyaremenko95@gmail.com>
2022-03-31 01:13:20 +0300

    fix

--- js/main_anim.js
+++ js/main_anim.js
@@ -621,14 +621,14 @@ if(document.fonts) {
     console.error('Sorry, unsupported browser');
 }
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    console.log(vh)
-});
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+// window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     console.log(vh)
+// });
 
 
 
